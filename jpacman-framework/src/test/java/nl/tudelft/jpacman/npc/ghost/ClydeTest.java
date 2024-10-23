f0566b6c6dea764e035588b2d4d3ac7c3561d573
Eline Mota <eline.mota01@gmail.com>
2024-10-23 14:37:47 +0200

    continuation Clyde

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -2,23 +2,34 @@ package nl.tudelft.jpacman.npc.ghost;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import nl.tudelft.jpacman.sprite.PacManSprites;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import nl.tudelft.jpacman.npc.ghost.Clyde;
-
+import nl.tudelft.jpacman.level.LevelFactory;
+import nl.tudelft.jpacman.npc.ghost.GhostMapParser;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class ClydeTest {
     private List<String> map;
-    private Clyde clyde;
+    private GhostFactory clyde;
+    private LevelFactory level;
+    private GhostMapParser mapParser;
+    private PacManSprites pcSprite;
 
 
     @Test
     void Essai(){
         map = Arrays.asList("############", "#P        C#", "############");
-        clyde = new Clyde();
+        pcSprite = new PacManSprites();
+        clyde = new GhostFactory(pcSprite);
+        clyde.createClyde();
+        level = new LevelFactory(pcSprite, clyde);
+        mapParser = new GhostMapParser();
+
+
 
     }
 
