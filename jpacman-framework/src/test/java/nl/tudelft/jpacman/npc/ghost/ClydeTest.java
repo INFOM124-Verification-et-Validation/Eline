6b2f4ab5e766ac95ba3ec06b0e658424043b1ec8
Eline Mota <eline.mota01@gmail.com>
2024-10-23 15:17:55 +0200

    continuation clyde

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -2,35 +2,40 @@ package nl.tudelft.jpacman.npc.ghost;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.sprite.PacManSprites;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import nl.tudelft.jpacman.npc.ghost.Clyde;
+import nl.tudelft.jpacman.npc.Ghost;
 import nl.tudelft.jpacman.level.LevelFactory;
+import nl.tudelft.jpacman.level.Level;
 import nl.tudelft.jpacman.npc.ghost.GhostMapParser;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class ClydeTest {
-    /**private List<String> map;
-    private GhostFactory clyde;
+    private List<String> map;
+    private GhostFactory ghost;
     private LevelFactory level;
     private GhostMapParser mapParser;
     private PacManSprites pcSprite;
+    private BoardFactory board;
 
 
     @Test
     void Essai(){
         map = Arrays.asList("############", "#P        C#", "############");
         pcSprite = new PacManSprites();
-        clyde = new GhostFactory(pcSprite);
-        clyde.createClyde();
-        level = new LevelFactory(pcSprite, clyde);
-        mapParser = new GhostMapParser();
+        ghost = new GhostFactory(pcSprite);
+        level = new LevelFactory(pcSprite, ghost);
+        board = new BoardFactory(pcSprite);
+        mapParser = new GhostMapParser(level, board, ghost);
+        Ghost clyde = ghost.createClyde();
+        Level mapfinal = mapParser.parseMap(map);
+        clyde.nextAiMove();
 
-
-
-    } **/
+    }
 
 }
